f7c0b2cd71e5c55021e6faa19af5d7c9df861c9e
bailu-ds <bailu-ds@gomeplus.com>
2017-10-23 18:37:29 +0800

    init

---
+++ my-cloud-service-hi/src/main/java/my/cloud/service/hi/App.java
@@ -0,0 +1,13 @@
+package my.cloud.service.hi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
